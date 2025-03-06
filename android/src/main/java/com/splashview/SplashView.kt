27bbd78dd713fb8937cb8cc4be85a0d43fbd4c6c
Jagnesh Chawla <jagnesh_chawla@hotmail.com>
2025-03-06 23:20:12 +0530

    fix(action): android fullscreen support added

--- android/src/main/java/com/splashview/SplashView.kt
+++ android/src/main/java/com/splashview/SplashView.kt
@@ -2,33 +2,45 @@ package com.splashview
 
 import android.app.Activity
 import android.app.Dialog
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 
 object SplashView {
-    private var splashDialog: Dialog? = null
-    fun showSplashView(activity: Activity) {
+  private var splashDialog: Dialog? = null
 
-      if (activity.isFinishing || activity.isDestroyed) {
-        println("Skipping showSplash: Activity is not ready.")
-        return
-      }
-
-      activity.runOnUiThread {
-        if (splashDialog?.isShowing == true) return@runOnUiThread
-        val inflater = LayoutInflater.from(activity)
-        val dialogView: View = inflater.inflate(R.layout.launch_screen, null)
-        val themeId = activity.resources.getIdentifier("SplashViewTheme", "style", activity.packageName)
-        splashDialog =if(themeId !=0)  Dialog(activity,R.style.SplashViewTheme) else Dialog(activity,R.style.SplashScreen_SplashTheme)
-        splashDialog?.setContentView(dialogView)
-        splashDialog?.setCancelable(false)
-        splashDialog?.show()
-      }
+  fun showSplashView(activity: Activity) {
+    if (activity.isFinishing || activity.isDestroyed) {
+      println("Skipping showSplash: Activity is not ready.")
+      return
     }
 
-    fun hideSplashView() {
-      splashDialog?.dismiss()
-      splashDialog = null
-    }
+    activity.runOnUiThread {
+      if (splashDialog?.isShowing == true) return@runOnUiThread
 
+      val inflater = LayoutInflater.from(activity)
+      val dialogView: View = inflater.inflate(R.layout.launch_screen, null)
+
+      val themeId = activity.resources.getIdentifier("SplashViewTheme", "style", activity.packageName)
+      val themeResId = if (themeId != 0) themeId else R.style.SplashScreen_SplashTheme
+
+      splashDialog = Dialog(activity, themeResId).apply {
+        setContentView(dialogView)
+        setCancelable(false)
+        window?.apply {
+          setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+          setBackgroundDrawableResource(android.R.color.transparent)
+          addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS) // Extend behind status bar
+        }
+        show()
+      }
+    }
+  }
+
+  fun hideSplashView() {
+    splashDialog?.takeIf { it.isShowing }?.dismiss()
+    splashDialog = null
+  }
 }
